468b7a223095d6d42a14579b09bc49fa25d6743e
Bayan Swaid <bayan.swaid.sk@gmail.com>
2019-12-17 14:43:10 +0200

    Completed step6 exercise

--- Exercises/Javascript-Basics-02/step 6/step6.js
+++ Exercises/Javascript-Basics-02/step 6/step6.js
@@ -1,15 +1,43 @@
-img1.onmouseleave = function() {
-  img1.setAttribute("src", "images/image1.jpg");
-};
-img2.onmouseleave = function() {
-  img2.setAttribute("src", "images/image2.jpg");
-};
-img3.onmouseleave = function() {
-  img3.setAttribute("src", "images/image3.jpg");
-};
-img4.onmouseleave = function() {
-  img4.setAttribute("src", "images/image4.jpg");
-};
-img5.onmouseleave = function() {
-  img5.setAttribute("src", "images/image5.jpg");
-};
+const img1 = document.querySelector("#image1");
+const img2 = document.querySelector("#image2");
+const img3 = document.querySelector("#image3");
+const img4 = document.querySelector("#image4");
+const img5 = document.querySelector("#image5");
+function change(image) {
+  switch (image) {
+    case 1:
+      img1.setAttribute("src", "images/image1_2.jpg");
+      break;
+    case 2:
+      img2.setAttribute("src", "images/image2_2.jpg");
+      break;
+    case 3:
+      img3.setAttribute("src", "images/image3_2.jpg");
+      break;
+    case 4:
+      img4.setAttribute("src", "images/image4_2.jpg");
+      break;
+    case 5:
+      img5.setAttribute("src", "images/image5_2.jpg");
+      break;
+  }
+}
+function leave(image) {
+  switch (image) {
+    case 1:
+      img1.setAttribute("src", "images/image1.jpg");
+      break;
+    case 2:
+      img2.setAttribute("src", "images/image2.jpg");
+      break;
+    case 3:
+      img3.setAttribute("src", "images/image3.jpg");
+      break;
+    case 4:
+      img4.setAttribute("src", "images/image4.jpg");
+      break;
+    case 5:
+      img5.setAttribute("src", "images/image5.jpg");
+      break;
+  }
+}

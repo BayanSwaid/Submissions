e542920d7bbd56ce2fdb13a9c527b37c7a294685
Bayan Swaid <bayan.swaid.sk@gmail.com>
2019-12-17 13:11:13 +0200

    Completed step5 exercise

---
+++ Exercises/Javascript-Basics-02/step 6/step6.js
@@ -0,0 +1,15 @@
+img1.onmouseleave = function() {
+  img1.setAttribute("src", "images/image1.jpg");
+};
+img2.onmouseleave = function() {
+  img2.setAttribute("src", "images/image2.jpg");
+};
+img3.onmouseleave = function() {
+  img3.setAttribute("src", "images/image3.jpg");
+};
+img4.onmouseleave = function() {
+  img4.setAttribute("src", "images/image4.jpg");
+};
+img5.onmouseleave = function() {
+  img5.setAttribute("src", "images/image5.jpg");
+};

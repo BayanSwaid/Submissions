7ce3c7606faeab72baf0efc93af10c0859b28ed5
Bayan Swaid <bayan.swaid.sk@gmail.com>
2020-01-16 12:06:21 +0200

    Creating Table

--- Exercises/SQLite Basic/answers.sh
+++ Exercises/SQLite Basic/answers.sh
@@ -1,3 +1,4 @@
+Basic Queries:
 "SELECT name FROM students"
 "SELECT * FROM students WHERE Age > 30"
 "SELECT * FROM students WHERE Age = 30 AND Gender = 'F'"
@@ -5,3 +6,10 @@
 "INSERT INTO students VALUES (null,'Bayan', 22, 'F', 400)"
 "UPDATE students SET Points = Points + 10 WHERE name='Basma'"
 "UPDATE students SET Points = Points - 20 WHERE name = 'Alex'"
+
+
+Creating Table:
+"INSERT INTO graduates (name, Age, Gender, Points)
+SELECT name, Age, Gender, Points FROM students WHERE name = 'Layal'"
+"UPDATE graduates SET Graduation = '08/09/2018' WHERE name = 'Layal'"
+"DELETE FROM students WHERE name='Layal'"

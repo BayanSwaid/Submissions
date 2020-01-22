a14d7756eb1b01096350b431cea9827500a62385
Bayan Swaid <bayan.swaid.sk@gmail.com>
2020-01-22 09:51:48 +0200

    Updated SQLite exercise

--- Exercises/SQLite Basic/answers.sh
+++ Exercises/SQLite Basic/answers.sh
@@ -1,4 +1,3 @@
-Basic Queries:
 "SELECT name FROM students"
 "SELECT * FROM students WHERE Age > 30"
 "SELECT * FROM students WHERE Age = 30 AND Gender = 'F'"
@@ -6,24 +5,13 @@ Basic Queries:
 "INSERT INTO students VALUES (null,'Bayan', 22, 'F', 400)"
 "UPDATE students SET Points = Points + 10 WHERE name='Basma'"
 "UPDATE students SET Points = Points - 20 WHERE name = 'Alex'"
-
-
-Creating Table:
 "INSERT INTO graduates (name, Age, Gender, Points)
 SELECT name, Age, Gender, Points FROM students WHERE name = 'Layal'"
 "UPDATE graduates SET Graduation = '08/09/2018' WHERE name = 'Layal'"
 "DELETE FROM students WHERE name='Layal'"
-
-
-Joins:
-
 "SELECT E.name as Employee, C.name as Company, C.Date from employees E JOIN companies C on E.Company = C.name"
 "SELECT E.name FROM employees E JOIN companies C on C.name = E.Company WHERE C.Date < 2000"
 "SELECT C.name FROM companies C JOIN employees E on Company = C.name WHERE Role = 'Graphic Designer'"
-
-
-Count & Filter:
-
 "SELECT name FROM students WHERE Points = (SELECT max(Points) FROM students)"
 "SELECT avg(Points) FROM students"
 "SELECT count(name) FROM students WHERE Points = 500"

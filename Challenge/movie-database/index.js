687f48d482b35139fc5a17fbd81ef8ba17b9587e
Bayan Swaid <bayan.swaid.sk@gmail.com>
2020-01-22 19:36:47 +0200

    Set up the basis for CRUD

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,24 +1,85 @@
 const express = require("express");
 const app = express();
-
+const movies = [{
+        title: 'Jaws',
+        year: 1975,
+        rating: 8
+    },
+    {
+        title: 'Avatar',
+        year: 2009,
+        rating: 7.8
+    },
+    {
+        title: 'Brazil',
+        year: 1985,
+        rating: 8
+    },
+    {
+        title: 'الإرهاب والكباب‎',
+        year: 1992,
+        rating: 6.2
+    }
+]
 app.get("/", (req, res) => {
-res.send("OK");
+    res.send("OK");
 });
 app.get("/test", (req, res) => {
-res.json({status:200, message:"ok"});
+    res.json({
+        status: 200,
+        message: "ok"
+    });
 });
 app.get("/time", (req, res) => {
-    res.json({status:200, message: new Date().getHours() + ":" + new Date().getMinutes() });
+    res.json({
+        status: 200,
+        message: new Date().getHours() + ":" + new Date().getMinutes()
     });
+});
 app.get("/hello/:ID", (req, res) => {
-    res.json({status:200, message:"hello " + req.params.ID});
-        });
+    res.json({
+        status: 200,
+        message: "hello " + req.params.ID
+    });
+});
 app.get("/search", (req, res) => {
-    if(req.query.s){
-    res.json({status:200, message:"ok", data:req.query.s});
-    }
-    else{
-        res.json({status:500, error:true, message:"you have to provide a search"});
+    if (req.query.s) {
+        res.json({
+            status: 200,
+            message: "ok",
+            data: req.query.s
+        });
+    } else {
+        res.json({
+            status: 500,
+            error: true,
+            message: "you have to provide a search"
+        });
     }
 });
-app.listen(2000, ()=> console.log("Server is listening to port 2000"));
+app.get("/movie/add", (req, res) => {
+    res.json({
+        status: 200,
+        message: "hello add"
+    });
+});
+app.get("/movie/get", (req, res) => {
+    res.json({
+        status: 200,
+        message: "hello get",
+        data: movies
+    });
+});
+app.get("/movie/edit", (req, res) => {
+    res.json({
+        status: 200,
+        message: "hello update"
+    });
+});
+app.get("/movie/delete", (req, res) => {
+    res.json({
+        status: 200,
+        message: "hello delete"
+    });
+});
+app.listen(2000, () => console.log("Server is listening to port 2000"));

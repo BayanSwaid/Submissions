e4f037958dc7250f6fabf034b34bf0febd212b65
Bayan Swaid <bayan.swaid.sk@gmail.com>
2020-01-22 12:48:10 +0200

    Create an express simple API

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -4,5 +4,12 @@ const app = express();
 app.get("/", (req, res) => {
 res.send("OK");
 });
+app.get("/test", (req, res) => {
+res.json({status:200, message:"ok"});
+});
+app.get("/time", (req, res) => {
+    res.json({status:200, message: "" + new Date().getHours() + ":" + new Date().getMinutes() });
+    });
+    
 
 app.listen(3000, () => console.log("Server is listening to port 3000"));
